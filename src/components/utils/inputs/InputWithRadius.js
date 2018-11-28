3e75d441e3381a3c2354baf16f8bc22edb411984
mezdru <=>
2018-11-28 16:21:55 +0100

    Create input outlined with border radius component

---
+++ src/components/utils/inputs/InputWithRadius.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './InputWithRadius.css';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+
+/**
+ * @param label : String
+ * @param value : variable
+ * @param onChange : function
+ */
+
+const styles = theme => ({
+    textField: {
+      marginLeft: theme.spacing.unit,
+      marginRight: theme.spacing.unit,  
+    },
+  });
+  
+
+class InputWithRadius extends React.Component {
+
+    render(){
+        const { classes } = this.props;
+
+        return(
+            <TextField
+            label={this.props.label}
+            className={`inputWithRadius ${classes.textField}`}
+            value={this.props.value}
+            onChange={this.props.onChange}
+            margin="normal"
+            variant="outlined"
+            />
+        );
+    }
+}
+
+export default withStyles(styles, { withTheme: true })(InputWithRadius);
