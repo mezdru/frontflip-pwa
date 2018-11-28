6044e38479395881d1a656b7a1a11fc5d8129de1
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2018-11-28 16:22:42 +0100

    Create input outlined with border radius component (#5)

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from '@material-ui/core/Button';
 import Login from '../components/login/Login';
+import InputWithRadius from '../components/utils/inputs/InputWithRadius';
 
 export class Home extends React.Component {
 
@@ -9,6 +10,7 @@ export class Home extends React.Component {
             <div>
                 This is Home
                 <Login/>
+                <InputWithRadius label='Your password' />
             </div>
         );
     }
