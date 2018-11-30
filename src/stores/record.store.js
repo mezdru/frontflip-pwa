26085842667f1a7f14359c291d80501e5e39aa9e
mezdru <=>
2018-11-30 14:05:55 +0100

    Correct MobX for new API routes

--- src/stores/record.store.js
+++ src/stores/record.store.js
@@ -6,43 +6,40 @@ class RecordStore {
     inProgress = false;
     errors = null;
     values = {
-        orgTag: '',
-        recordTag: '',
+        orgId: '',
+        recordId: '',
         record: {}
     };
 
-    setOrgTag(orgTag) {
-        this.values.orgTag = orgTag;
+    setOrgId(orgId) {
+        this.values.orgId = orgId;
     }
-    setRecordTag(recordTag) {
-        this.values.recordTag = recordTag;
+    setRecordId(recordId) {
+        this.values.recordId = recordId;
     }
     setRecord(record) {
         this.values.record = record;
     }
 
     reset() {
-        this.values.orgTag = '';
-        this.values.recordTag = '';
+        this.values.orgId = '';
+        this.values.recordId = '';
         this.values.record = {};
     }
 
-    /**
-     * @description Get record by orgTag and recordTag
-     */
-    getRecordByTag() {
+    getRecord() {
         this.inProgress = true;
         this.errors = null;
 
-        return agent.Record.get(this.values.orgTag, this.values.recordTag)
-            .then(data => { this.values.record = data.record;})
+        return agent.Record.get(this.values.recordId)
+            .then(data => { this.values.record = data? data.record : {};})
             .catch(action((err) => {
                 this.errors = err.response && err.response.body && err.response.body.errors;
                 throw err;
             }))
             .finally(action(()=> { this.inProgress = false; }));
     }
-
+    
     /**
      * @description Post new record
      */
@@ -50,8 +47,8 @@ class RecordStore {
         this.inProgress = true;
         this.errors = null;
 
-        return agent.Record.post(this.values.orgTag, this.values.record)
-            .then(data => { this.values.record = data.record;})
+        return agent.Record.post(this.values.orgId, this.values.record)
+            .then(data => { this.values.record = data? data.record : {};})
             .catch(action((err) => {
                 this.errors = err.response && err.response.body && err.response.body.errors;
                 throw err;
@@ -66,24 +63,8 @@ class RecordStore {
         this.inProgress = true;
         this.errors = null;
 
-        return agent.Record.put(this.values.orgTag, this.values.recordTag, this.values.record)
-            .then(data => { this.values.record = data.record;})
-            .catch(action((err) => {
-                this.errors = err.response && err.response.body && err.response.body.errors;
-                throw err;
-            }))
-            .finally(action(()=> { this.inProgress = false; }));
-    }
-
-    /**
-     * @description Get my record
-     */
-    getMyRecord() {
-        this.inProgress = true;
-        this.errors = null;
-
-        return agent.Record.getMe(this.values.orgTag)
-            .then(data => { this.values.record = data.record;})
+        return agent.Record.put(this.values.orgId, this.values.recordId, this.values.record)
+            .then(data => { this.values.record = data? data.record : {};})
             .catch(action((err) => {
                 this.errors = err.response && err.response.body && err.response.body.errors;
                 throw err;
@@ -94,12 +75,15 @@ class RecordStore {
     /**
      * @description Delete my record
      */
-    deleteMyRecord() {
+    deleteRecord() {
         this.inProgress = true;
         this.errors = null;
 
-        return agent.Record.deleteMe(this.values.orgTag)
-            .then(data => { this.values.record = data.record;})
+        return agent.Record.delete(this.values.recordId)
+            .then(data => { 
+                this.values.record = {}; 
+                this.values.recordId = '';
+            })
             .catch(action((err) => {
                 this.errors = err.response && err.response.body && err.response.body.errors;
                 throw err;
@@ -113,15 +97,14 @@ decorate(RecordStore, {
     inProgress: observable,
     errors: observable,
     values: observable,
-    setOrgTag: action,
+    setOrgId: action,
     reset: action,
-    setRecordTag: action,
+    setrecordId: action,
     setRecord: action,
-    getRecordByTag: action,
+    getRecord: action,
     postRecord: action,
     updateRecord: action,
-    getMyRecord: action,
-    deleteMyRecord: action
+    deleteRecord: action
 });
 
 export default new RecordStore();
