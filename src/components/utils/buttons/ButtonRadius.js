faa82f2e2186c277ae51f244b9e7e322240469be
mezdru <=>
2018-12-03 14:33:09 +0100

    minor update

--- src/components/utils/buttons/ButtonRadius.js
+++ src/components/utils/buttons/ButtonRadius.js
@@ -2,12 +2,6 @@ import React from 'react';
 import './ButtonRadius.css';
 import Button from '@material-ui/core/Button';
 
-/**
- * @param label : String
- * @param value : variable
- * @param onChange : function
- */
-
 const ButtonRadius = ({...rest}) =>
     <Button className={`buttonRadius`} variant="contained" {...rest}/>;
 
