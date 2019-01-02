77d06d9dffabcd2c99027772077a4bd0b4afefad
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-01-02 18:09:03 +0100

    Persists locale for the whole app (#67)
    * Create URL service to populate URL
    
    * Persist locale for all app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,29 +1,15 @@
 import React from 'react'
 import AuthPage from './auth/AuthPage';
 import {inject, observer} from 'mobx-react';
-import { matchPath } from 'react-router'
-
-
 
 let Home = inject("organisationStore", "commonStore")(observer(class Home extends React.Component {
 
     constructor(props){
         super(props);
-
-        // Due to a bug of react-router-dom, url parameters are not transmetted to nested routes.
-        const matchLocale = matchPath(this.props.history.location.pathname, {
-            path: '/:locale',
-            exact: false,
-            strict: false
-          });
-
-          this.props.commonStore.setCookie('locale',matchLocale.params.locale)
-
     }
 
     componentDidMount() {
         if(this.props.match.params.organisationTag) {
-            console.log(this.props.match.params.organisationTag);
             this.props.organisationStore.setOrgTag(this.props.match.params.organisationTag);
             this.props.organisationStore.getOrganisationForPublic();
         }
