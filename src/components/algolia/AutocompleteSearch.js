01966e29c9ace6a81c36908d95bd695ac2480d80
mezdru <=>
2019-01-15 10:24:21 +0100

    Clean some css and jss

--- src/components/algolia/AutocompleteSearch.js
+++ src/components/algolia/AutocompleteSearch.js
@@ -6,54 +6,7 @@ import MaterialSearchBox from "./MaterialSearchBox";
 import { observe, autorun } from 'mobx';
 import { StickyContainer, Sticky } from 'react-sticky';
 import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
-
-const styles = theme => ({
-    hitListContainer: {
-        position: 'relative',
-        top: 60,
-        [theme.breakpoints.up('md')]: {
-            top: 150,
-        },
-        width: '100%',
-    },
-    hitList: {
-        width: '100%',
-        '& ul': {
-            listStyleType: 'none',
-            padding:0,
-            marginTop: '32px',
-            marginBottom: '32px',
-        },
-        '& ul li': {
-            marginBottom: '32px'
-        },
-        '& ul li > div:first-child' : {
-            position: 'relative',
-            left: '0',
-            right: '0',
-            margin: 'auto'
-        }
-    },
-    fullWidth: {
-        width: '100%'
-    },
-    searchBar: {
-        left: 0,
-        right: 0,
-        margin: 'auto',
-        zIndex: 1000,
-        marginTop:21,
-        background: 'transparent',
-    },
-    searchBarMarginTop: {
-        position: 'static',
-        marginTop:59, // 147 - 24 - 64
-        [theme.breakpoints.up('md')]: {
-            marginTop: 151, //239 - 24 - 64
-        },
-        width: '100%',
-    }
-});
+import {styles} from './AutocompleteSearch.css'
 
 class AutocompleteSearch extends Component {
     constructor(props) {
@@ -146,8 +99,6 @@ class AutocompleteSearch extends Component {
                 <CircularProgress color="primary"/>
             )
         }
-
-
     }
 }
 
