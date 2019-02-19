8e0ba9c69c32fc750036c32569dce4e7d72a02f6
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-02-19 17:23:50 +0100

    Implements demo for auto-save logic (#259)

--- src/components/onboard/steps/OnboardIntro.js
+++ src/components/onboard/steps/OnboardIntro.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { withStyles } from '@material-ui/core';
 import { inject, observer } from "mobx-react";
+import { TextField } from '@material-ui/core'
 
 class OnboardIntro extends React.Component {
   constructor(props) {
@@ -9,12 +10,39 @@ class OnboardIntro extends React.Component {
     };
   }
 
+  handleChange = (e, field) => {
+    let record = this.props.recordStore.values.record;
+    record[field] = e.target.value;
+    this.props.recordStore.setRecord(record);
+    this.forceUpdate(); // why component do not update auto like Login fields ?
+  }
+
   render() {
     const {record} = this.props.recordStore.values;
 
     return (
       <div>
         This is intro component
+        <TextField
+          label="Name"
+          type="text"
+          fullWidth
+          variant={"outlined"}
+          value={record.name}
+          onChange={(e) => this.handleChange(e, 'name')}
+          onBlur={this.props.handleSave}
+          required
+        />
+        <TextField
+          label="Intro"
+          type="text"
+          fullWidth
+          variant={"outlined"}
+          value={record.intro}
+          onChange={(e) => this.handleChange(e, 'intro')}
+          onBlur={this.props.handleSave}
+          required
+        />
       </div>
     );
   }
