d88e341840fe6675f2da2d0a3ff7de9269974f29
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-03-05 14:23:18 +0100

    Handle algolia service errors  (#301)

--- src/services/algolia.service.js
+++ src/services/algolia.service.js
@@ -41,7 +41,7 @@ class AlgoliaService {
   }
 
   fetchFacetValues(lastSelection, privateOnly, filters, query) {
-    if(!index) return Promise.resolve();
+    if(!this.index) return Promise.resolve();
     return new Promise((resolve, reject) => {
       this.index.searchForFacetValues({
         facetName: 'hashtags.tag',
@@ -65,7 +65,7 @@ class AlgoliaService {
   }
 
   loadBank(filters) {
-    if(!index) return Promise.resolve();
+    if(!this.index) return Promise.resolve();
     return new Promise((resolve, reject) => {
       if(!filters && commonStore.getLocalStorage('wingsBank', true)) resolve();
       this.index.search({
@@ -93,7 +93,7 @@ class AlgoliaService {
   }
 
   fetchOptions(inputValue){
-    if(!index) return Promise.resolve();
+    if(!this.index) return Promise.resolve();
     return new Promise((resolve, reject) => {
       this.index.search({
           query: inputValue,
@@ -108,7 +108,7 @@ class AlgoliaService {
 
 
   fetchHits(filters, query, facetFilters, page) {
-    if(!index) return Promise.resolve();
+    if(!this.index) return Promise.resolve();
     return new Promise((resolve, reject) => {
       this.index.search({
         page : page || 0,
