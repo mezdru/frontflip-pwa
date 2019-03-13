55ad1ed96930ef514bbe1f0303fc84f598626e51
Quentin <quentin.drumez@isen.yncrea.fr>
2019-03-13 11:38:27 +0100

    Onboard : intro & first Wings responsive

--- src/components/onboard/steps/OnboardFirstWings.js
+++ src/components/onboard/steps/OnboardFirstWings.js
@@ -29,6 +29,10 @@ const styles = theme => ({
     display: 'inline-block',
     width: 169,
     height: 169,
+    [theme.breakpoints.down('sm')]: {
+      width: 130,
+      height: 130,
+    },
     overflow: 'hidden',
     padding: 16,
     margin: 8,
@@ -41,6 +45,11 @@ const styles = theme => ({
       width: 145,
       height: 145,
       borderRadius: 75,
+      [theme.breakpoints.down('sm')]: {
+        width: 100,
+        height: 100,
+        borderRadius: 50,
+      },
       outline: 'none',
     },
     '& div': {
