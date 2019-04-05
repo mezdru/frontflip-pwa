2cd7538ada0b52a4740610b80cb43240836f8bcf
Quentin <quentin.drumez@isen.yncrea.fr>
2019-04-05 13:57:12 +0200

    Migrate sw-precache to workbox

--- workbox.config.js
+++ workbox.config.js
@@ -1,4 +1,28 @@
-module.exports = options => {
-  options.skipWaiting = true;
-  return options;
+module.exports = {
+  globDirectory: 'build/',
+  globPatterns: [
+    '**/*.{html,json,js,css}'
+  ],
+  skipWaiting: true,
+  cleanupOutdatedCaches: true,
+  swDest: 'build/service-worker.js',
+
+  // Define runtime caching rules.
+  runtimeCaching: [{
+    // Match any request ends with .png, .jpg, .jpeg or .svg.
+    urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+
+    // Apply a cache-first strategy.
+    handler: 'CacheFirst',
+
+    options: {
+      // Use a custom cache name.
+      cacheName: 'images',
+
+      // Only cache 10 images.
+      expiration: {
+        maxEntries: 10,
+      },
+    },
+  }],
 };
