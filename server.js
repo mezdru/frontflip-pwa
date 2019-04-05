9b710aec295da1935ca9856b90527e421ebcafb8
Quentin <quentin.drumez@isen.yncrea.fr>
2019-04-05 13:13:08 +0200

    Server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/ping', function (req, res) {
+ return res.send('pong');
+});
+app.get('/*', function (req, res) {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate, max-age=0');
+  res.setHeader('Clear-Site-Data', 'cache, storage');
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+app.listen(port);
