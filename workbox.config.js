13fe43d384bba5336e42353d315d6433b778eadd
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-04-05 17:43:14 +0200

    Workbox config (#490)

--- workbox.config.js
+++ workbox.config.js
@@ -1,28 +1,49 @@
 module.exports = {
   globDirectory: 'build/',
-  globPatterns: [
-    '**/*.{html,json,js,css}'
-  ],
+  // globPatterns: [
+  //   '**/*.{html,json,js,css}'
+  // ],
   skipWaiting: true,
+  clientsClaim: true,
   cleanupOutdatedCaches: true,
   swDest: 'build/service-worker.js',
+  maximumFileSizeToCacheInBytes: 5000000,
 
-  // Define runtime caching rules.
-  runtimeCaching: [{
-    // Match any request ends with .png, .jpg, .jpeg or .svg.
-    urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
-
-    // Apply a cache-first strategy.
-    handler: 'CacheFirst',
-
-    options: {
-      // Use a custom cache name.
-      cacheName: 'images',
-
-      // Only cache 10 images.
-      expiration: {
-        maxEntries: 10,
-      },
+  runtimeCaching: [
+    {
+      urlPattern: /\.(?:html|json|js|css)$/,
+      handler: 'NetworkFirst',
+      options: {
+        cacheName: 'global',
+        cacheableResponse: {
+          statuses: [0, 200]
+        },
+        expiration: {
+          maxEntries: 30,
+          maxAgeSeconds: 60,
+        },
+      }
     },
-  }],
+    {
+        // Match any request ends with .png, .jpg, .jpeg or .svg.
+        urlPattern: /\.(?:png|jpg|jpeg|svg)$/,
+    
+        // Apply a cache-first strategy.
+        handler: 'CacheFirst',
+    
+        options: {
+          // Use a custom cache name.
+          cacheName: 'images',
+          cacheableResponse: {
+            statuses: [0, 200]
+          },
+    
+    
+          // Only cache 10 images.
+          expiration: {
+            maxEntries: 50,
+          },
+        },
+      }
+  ]
 };
