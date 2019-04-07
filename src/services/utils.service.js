b330da3ff209160332a521218fd895c0c8292f13
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-04-07 14:16:26 +0200

    Create an utils methods service (#491)

---
+++ src/services/utils.service.js
@@ -0,0 +1,17 @@
+// Example of Constants which can be declared here.
+const someCommonValues = ['common', 'values'];
+
+/**
+ * @description Randomize the order of elements in an array.
+ * @param {Array} array 
+ * @author Quentin Drumez
+ */
+export const shuffleArray = (array) => {
+  let arrayOut = [];
+  while(array.length !== 0) {
+    let randomIndex = Math.floor(Math.random() * array.length);
+    arrayOut.push(array[randomIndex]);
+    array.splice(randomIndex, 1);
+  }
+  return arrayOut;
+}
