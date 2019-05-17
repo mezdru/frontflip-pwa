8cc0845c1cd3d5526ca274ea3b1ffe93f1e27fd3
mezdru <quentin.drumez@isen.yncrea.fr>
2019-05-17 19:03:32 +0200

    Search page : details

--- src/components/search/SearchSuggestions.js
+++ src/components/search/SearchSuggestions.js
@@ -20,10 +20,10 @@ const styles = theme => ({
   suggestion: {
     margin: 8,
     marginBottom: 20,
-    background: 'white',
+    background: 'rgba(255,255,255, .9)',
     color: theme.palette.primary.dark,
     '&:hover': {
-      background: 'rgb(220,220,220)'
+      background: 'rgba(220,220,220, .9)'
     },
     opacity: 0,
     animation: 'easeIn .6s',
