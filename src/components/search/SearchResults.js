a32aece287eb9fec321f697e50e49ce9324aa200
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-06-07 16:48:33 +0200

    Fix: search results : infinite scroll (#612)

--- src/components/search/SearchResults.js
+++ src/components/search/SearchResults.js
@@ -41,7 +41,7 @@ const styles = theme => ({
     },
   },
   sentinel: {
-    position: 'relative',
+    position: 'absolute',
     marginTop: '-500px',
   }
 });
@@ -116,7 +116,7 @@ class SearchResults extends React.Component {
   }
 
   fetchHits = (filters, query, facetFilters, page) => {
-    AlgoliaService.fetchHits(filters, query, facetFilters, page, true, ((!page || page === 0) ? 5 : 15))
+    AlgoliaService.fetchHits(filters, query, facetFilters, page, true, 5)
       .then((content) => {
 
         if ((!content || !content.hits || content.hits.length === 0) && (!page || page === 0)) this.setState({ showNoResult: true, hideShowMore: true });
@@ -166,7 +166,7 @@ class SearchResults extends React.Component {
               </li>
             );
           })}
-          <div id="algolia-sentinel"></div>
+          <div id="algolia-sentinel" className={classes.sentinel}></div>
 
           {!hideShowMore && (
             <li>
