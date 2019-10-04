a172c55efcefad3f1e6af023a9cfa468336b50a9
mezdru <quentin.drumez@isen.yncrea.fr>
2019-10-04 14:32:42 +0200

    Update title, emoji and send slack notif

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Grid, withStyles, Typography, Button } from '@material-ui/core';
 import { inject, observer } from "mobx-react";
 import Header from '../components/header/Header';
@@ -7,6 +7,8 @@ import ProfileService from '../services/profile.service';
 import UrlService from '../services/url.service';
 import ReactGA from 'react-ga';
 import SlackService from '../services/slack.service';
+import { Redirect } from 'react-router-dom';
+import Intercom from 'react-intercom';
 
 console.debug('Loading WelcomePage');
 
@@ -15,8 +17,8 @@ ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
 const styles = theme => ({
   layout: {
     height: '100vh',
-    left:0,
-    right:0,
+    left: 0,
+    right: 0,
     margin: 'auto',
     [theme.breakpoints.down('xs')]: {
       height: 'unset',
@@ -51,15 +53,25 @@ const styles = theme => ({
 
 class WelcomePage extends React.Component {
 
+  state = {
+    redirectTo: null
+  }
+
   componentDidMount() {
+    if (!this.props.userStore.values.currentUser) return this.setState({ redirectTo: '/' + this.props.commonStore.locale + '/signin' });
+    let { email, _id } = this.props.userStore.values.currentUser;
+    let { locale } = this.props.commonStore;
+
     ReactGA.pageview(window.location.pathname);
-    SlackService.notify('#alert')
+    SlackService.notify('#alerts',
+      `An User is connected without organisation. (locale: ${locale} ) (email: ${email.value} ) (id: ${_id} )`);    
   }
 
   render() {
     const { classes } = this.props;
     const { currentUser } = this.props.userStore.values;
-    const { orgTag } = this.props.organisationStore.values;
+
+    if (this.state.redirectTo) return (<Redirect to={this.state.redirectTo} />);
 
     return (
       <div>
@@ -67,7 +79,7 @@ class WelcomePage extends React.Component {
         <main>
           <Grid container item className={classes.layout} alignItems={'center'} justify={'center'} direction="column" xs={12} md={6} >
             <Grid item className={classes.item}>
-              <img className={classes.bigPicture} src={ProfileService.getEmojiUrl('😀')} alt="bigPicture" />
+              <img className={classes.bigPicture} src={ProfileService.getEmojiUrl('🤔')} alt="bigPicture" />
             </Grid>
             <Grid item className={classes.item} >
               <Typography variant={'h1'} className={classes.title}>
@@ -97,13 +109,21 @@ class WelcomePage extends React.Component {
               </Grid>
             </Grid>
           </Grid>
+
+          <Suspense fallback={<></>}>
+            <Intercom
+              appID={"k7gprnv3"}
+              user_id={currentUser._id}
+              email={currentUser.email ? currentUser.email.value : (currentUser.google ? currentUser.google.email : null)}
+            />
+          </Suspense>
         </main>
       </div>
     );
   }
 }
 
-export default inject('commonStore', 'organisationStore', 'authStore', 'recordStore', 'userStore')(
+export default inject('commonStore', 'userStore')(
   observer(
     withStyles(styles, { withTheme: true })(WelcomePage)
   )
