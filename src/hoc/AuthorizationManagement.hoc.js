b5dfa013e903c64ffd8131f845595ebf1de94a50
Quentin Drumez <quentin.drumez@isen.yncrea.fr>
2019-10-28 17:20:16 +0100

    Add user to keen events (#800)

--- src/hoc/AuthorizationManagement.hoc.js
+++ src/hoc/AuthorizationManagement.hoc.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import {observer, inject} from 'mobx-react';
 import {Redirect} from 'react-router-dom';
 import { getBaseUrl } from "../services/utils.service";
+import undefsafe from 'undefsafe';
 
 const COMPONENTS = ['onboard', 'search'];
 const withAuthorizationManagement = (ComponentToWrap, componentName) => {
@@ -29,6 +30,12 @@ const withAuthorizationManagement = (ComponentToWrap, componentName) => {
       if(isAuth && !this.hasValidatedEmail() && !this.props.userStore.currentUser.googleUser && !this.props.userStore.currentUser.linkedinUser ) return <Redirect push to={'/' + locale + '/error/403/email'} />;
       if(isAuth && !this.hasAccessToOrg()) return <Redirect push to={'/' + locale + '/error/403/organisation'} />;
       if(!isAuth && !this.hasAccessToOrg()) return <Redirect push to={baseUrl + '/signin'} />;
+
+      if(isAuth && this.belongsToOrg() && !this.props.commonStore.getCookie('isAccessInLogged')) {
+        this.props.commonStore.setCookie('isAccessInLogged', true, (new Date((new Date()).getTime() + 60*60000))) // 1 hour
+        this.props.keenStore.recordEvent('accessIn', {recordEmitter: undefsafe(this.props.recordStore.currentUserRecord, '_id')});
+      }
+
       if(isAuth && componentName === COMPONENTS[1] && this.needOnboarding()) return <Redirect push to={baseUrl + '/onboard'} />;
       if(componentName === COMPONENTS[0] && !this.belongsToOrg()) return <Redirect push to={baseUrl} />;
 
@@ -36,7 +43,7 @@ const withAuthorizationManagement = (ComponentToWrap, componentName) => {
     }
   }
 
-  AuthorizationManagement = inject('orgStore', 'userStore', 'authStore')(observer(AuthorizationManagement));
+  AuthorizationManagement = inject('orgStore', 'userStore', 'authStore', 'commonStore', 'keenStore', 'recordStore')(observer(AuthorizationManagement));
   return AuthorizationManagement;
 }
 export default withAuthorizationManagement;
