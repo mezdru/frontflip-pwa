4cd5a17b554939ed55eaf43d221bc372e4a023b4
mezdru <quentin.drumez@isen.yncrea.fr>
2019-11-20 10:27:57 +0100

    Onboard suggestions : loader

--- src/components/search/OnboardSuggestions.js
+++ src/components/search/OnboardSuggestions.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Grid, withStyles } from '@material-ui/core';
+import { Grid, withStyles, CircularProgress, Typography } from '@material-ui/core';
 import { inject, observer } from 'mobx-react';
 import Wings from '../utils/wing/Wings';
 import ProfileService from '../../services/profile.service';
 import SuggestionsService from '../../services/suggestions.service';
-import { injectIntl } from "react-intl";
+import { injectIntl, FormattedMessage } from "react-intl";
 import { observe } from 'mobx';
 import { getUnique } from '../../services/utils.service';
 
@@ -30,7 +30,21 @@ const styles = theme => ({
       marginBottom: 10,
     },
     marginLeft: -8,
-    overflow: 'hidden'
+    overflow: 'hidden',
+    position: 'relative',
+    width: '100%'
+  },
+  loaderContainer: {
+    position: 'absolute',
+    display: 'flex',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    top: '50%',
+    left: '50%',
+    transform: 'translateX(-50%) translateY(-50%)'
+  },
+  loaderText: {
+    paddingLeft: 16
   }
 });
 
@@ -52,18 +66,18 @@ class OnboardSuggestions extends React.Component {
     this.unsubscribeUserQuery = observe(this.props.searchStore.values, 'userQuery', () => {
       // If user write a word, we wait that he finishes before calling algolia.
       clearTimeout(timeout);
-      if(!this.fetchSuggestionsAfterSelectInProgress)
-        timeout = setTimeout(() => {this.fetchSuggestions(null, this.props.wingsFamily)}, SUGGESTIONS_QUERY_DELAY);
+      if (!this.fetchSuggestionsAfterSelectInProgress)
+        timeout = setTimeout(() => { this.fetchSuggestions(null, this.props.wingsFamily) }, SUGGESTIONS_QUERY_DELAY);
     });
 
     this.unsubscribeUserRecord = observe(record, (change) => {
-      if(change.name === 'hashtags' && change.oldValue.length > change.newValue.length) {
+      if (change.name === 'hashtags' && change.oldValue.length > change.newValue.length) {
         let lastRemoved = change.oldValue[change.oldValue.length - 1];
         this.fetchSuggestions(lastRemoved, this.props.wingsFamily, true);
       }
     });
   }
-  
+
   componentWillUnmount() {
     this.isUnmount = true;
     this.unsubscribeUserQuery();
@@ -71,12 +85,12 @@ class OnboardSuggestions extends React.Component {
   }
 
   fetchSuggestions = async (lastSelected, wingsFamily, includeLastSelected) => {
-    if(lastSelected && lastSelected.tag) this.fetchSuggestionsAfterSelectInProgress = false;
-    if(this.fetchSuggestionsAfterSelectInProgress) return;
-    let suggestions = await SuggestionsService.getSuggestions(lastSelected, this.props.searchStore.values.userQuery || '', wingsFamily );
-    if(lastSelected && !includeLastSelected) suggestions = suggestions.filter(suggestion => suggestion.tag !== lastSelected.tag);
-    if(this.isUnmount) return;
-    this.setState({suggestions: getUnique(suggestions, "tag"), lastSelected: (includeLastSelected ? {} : this.state.lastSelected)});
+    if (lastSelected && lastSelected.tag) this.fetchSuggestionsAfterSelectInProgress = false;
+    if (this.fetchSuggestionsAfterSelectInProgress) return;
+    let suggestions = await SuggestionsService.getSuggestions(lastSelected, this.props.searchStore.values.userQuery || '', wingsFamily);
+    if (lastSelected && !includeLastSelected) suggestions = suggestions.filter(suggestion => suggestion.tag !== lastSelected.tag);
+    if (this.isUnmount) return;
+    this.setState({ suggestions: getUnique(suggestions, "tag"), lastSelected: (includeLastSelected ? {} : this.state.lastSelected) });
   }
 
   shouldDisplaySuggestion = (suggestion) => {
@@ -88,9 +102,9 @@ class OnboardSuggestions extends React.Component {
   }
 
   shouldComponentUpdate(nextProps, nextState) {
-    return  (JSON.stringify(this.state.suggestions) !== JSON.stringify(nextState.suggestions)) || 
-            (JSON.stringify(nextProps.wingsFamily) !== JSON.stringify(this.props.wingsFamily)) ||
-            (this.state.suggestions.length === 0 && nextState.suggestions.length === 0)
+    return (JSON.stringify(this.state.suggestions) !== JSON.stringify(nextState.suggestions)) ||
+      (JSON.stringify(nextProps.wingsFamily) !== JSON.stringify(this.props.wingsFamily)) ||
+      (this.state.suggestions.length === 0 && nextState.suggestions.length === 0)
   }
 
   componentWillReceiveProps(nextProps) {
@@ -99,7 +113,7 @@ class OnboardSuggestions extends React.Component {
 
   handleSelectSuggestion = (suggestion) => {
     this.fetchSuggestionsAfterSelectInProgress = true;
-    this.setState({ lastSelected: suggestion }, () => {
+    this.setState({ lastSelected: suggestion, suggestions: [] }, () => {
       this.props.searchStore.setUserQuery('');
       this.fetchSuggestions(suggestion, this.props.wingsFamily);
       this.props.onSelect(suggestion);
@@ -107,7 +121,7 @@ class OnboardSuggestions extends React.Component {
   }
 
   handleCreateWing = () => {
-    this.props.handleCreateWing({name: this.props.searchStore.values.userQuery});
+    this.props.handleCreateWing({ name: this.props.searchStore.values.userQuery });
     this.props.searchStore.setUserQuery('');
   }
 
@@ -123,11 +137,17 @@ class OnboardSuggestions extends React.Component {
         <Grid item className={classes.suggestionsContainer}>
           {userQuery && (
             <Wings
-              src={ProfileService.getPicturePath({emoji: '💬'})}
-              label={this.props.intl.formatHTMLMessage({id: "onboard.createWing"}) + ' ' + ProfileService.htmlDecode(userQuery)}
+              src={ProfileService.getPicturePath({ emoji: '💬' })}
+              label={this.props.intl.formatHTMLMessage({ id: "onboard.createWing" }) + ' ' + ProfileService.htmlDecode(userQuery)}
               mode="button"
               onClick={this.handleCreateWing}
             />
+          )}
+          {suggestions.length === 0 && !userQuery && (
+            <div className={classes.loaderContainer} >
+              <div><CircularProgress color="secondary" /></div>
+              <div className={classes.loaderText} ><FormattedMessage id="onboard.suggestions.loading" /></div>
+            </div>
           )}
           {suggestions.map((suggestion, index) => {
             if (!this.shouldDisplaySuggestion(suggestion)) return null;
@@ -138,7 +158,7 @@ class OnboardSuggestions extends React.Component {
               <div
                 style={{ animationDelay: ((suggestionsDisplayed - 1) * 0.05) + 's' }}
                 className={classes.suggestion}
-                key={Math.random()} 
+                key={Math.random()}
               >
                 <Wings
                   label={ProfileService.getWingDisplayedName(suggestion, locale)}
@@ -157,5 +177,5 @@ class OnboardSuggestions extends React.Component {
 }
 
 export default inject('commonStore', 'recordStore', 'searchStore')(
-  observer(withStyles(styles)( injectIntl(OnboardSuggestions)))
+  observer(withStyles(styles)(injectIntl(OnboardSuggestions)))
 );
