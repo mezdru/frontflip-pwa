4b24e1b93e94f9ecde7db0214db988515b485df4
mezdru <quentin.drumez@isen.yncrea.fr>
2019-12-05 12:48:01 +0100

    Update propose Wings FAB

--- src/components/utils/buttons/SkillsPropositionFab.js
+++ src/components/utils/buttons/SkillsPropositionFab.js
@@ -1,18 +1,45 @@
-import React from 'react';
-import { Fab, withStyles } from '@material-ui/core';
-import { LibraryAdd } from '@material-ui/icons';
-import classNames from 'classnames';
+import React from "react";
+import { Fab, withStyles, Tooltip, withWidth } from "@material-ui/core";
+import { Add } from "@material-ui/icons";
+import classNames from "classnames";
+import { injectIntl, FormattedMessage } from "react-intl";
 
 class SkillsPropositionFab extends React.Component {
+  getVariant = () => {
+    switch (this.props.width) {
+      case "xs":
+      case "sm":
+        return null;
+      default:
+        return "extended";
+    }
+  };
 
   render() {
-    return(
-      <Fab color="secondary" aria-label="Propose Skills" className={classNames(this.props.className)} onClick={this.props.onClick} >
-        <LibraryAdd fontSize="large" />
-      </Fab>
+    return (
+      <Tooltip
+        title={this.props.intl.formatMessage({
+          id: "skillsProposition.fab.tooltip"
+        })}
+        placement="left"
+      >
+        <Fab
+          color="secondary"
+          aria-label="Propose Skills"
+          className={classNames(this.props.className)}
+          onClick={this.props.onClick}
+          variant={this.getVariant()}
+        >
+          <Add fontSize="large" />
+          {this.getVariant() === "extended" && (
+            <FormattedMessage id="skillsProposition.fab.tooltip" />
+          )}
+        </Fab>
+      </Tooltip>
     );
   }
 }
 
-
-export default withStyles(null, {withTheme: true})(SkillsPropositionFab);
+export default withWidth()(
+  withStyles(null, { withTheme: true })(injectIntl(SkillsPropositionFab))
+);
