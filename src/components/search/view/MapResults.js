1d7c081e3f0b8b170e6fe7e474bf410278942bdd
mezdru <quentin.drumez@isen.yncrea.fr>
2019-12-19 15:32:32 +0100

    Add marker & fix marker move on zoom

--- src/components/search/view/MapResults.js
+++ src/components/search/view/MapResults.js
@@ -1,5 +1,6 @@
 import React from "react";
 import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { withStyles } from "@material-ui/core";
 
 import { styles } from "./MapResults.css";
@@ -14,10 +15,21 @@ class MapResults extends React.Component {
   componentDidMount() {
     var map = new mapboxgl.Map({
       container: "results-map",
-      style: "mapbox://styles/mapbox/streets-v11"
+      style: "mapbox://styles/mapbox/streets-v11",
+      zoom: 5,
+      center: [2.349014, 48.864716],
+      renderWorldCopies: false
     });
 
-    this.setState({ map: map });
+    this.setState({ map: map }, () => {
+      this.addRecordMarker({}, 48.864716, 2.349014);
+    });
+  }
+
+  addRecordMarker = (record, lat, lng) => {
+    let marker = new mapboxgl.Marker()
+      .setLngLat([lng, lat])
+      .addTo(this.state.map);
   }
 
   render() {
@@ -25,7 +37,7 @@ class MapResults extends React.Component {
 
     return (
       <div className={classes.viewContainer}>
-        <div id="results-map" style={{height: 'calc(100vh - 183px'}}></div>;
+        <div id="results-map" style={{height: 'calc(100vh - 183px)'}}></div>
       </div>
     );
   }
