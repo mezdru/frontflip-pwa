d15d23167e24a16d7348faae31c61b94579c5d47
mezdru <quentin.drumez@isen.yncrea.fr>
2019-12-19 15:08:35 +0100

    Create map view for search results

---
+++ src/components/search/view/MapResults.css.js
@@ -0,0 +1,11 @@
+export const styles = theme => ({
+  viewContainer: {
+    position: "relative",
+    zIndex: 999,
+    width: "100%",
+    minHeight: "calc(100vh - 129px)",
+    padding: 16,
+    backgroundColor: "#f2f2f2",
+    background: 'black !important'
+  }
+});
--- src/components/search/view/MapResults.js
+++ src/components/search/view/MapResults.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React from "react";
+import mapboxgl from "mapbox-gl/dist/mapbox-gl.js";
+import { withStyles } from "@material-ui/core";
+
+import { styles } from "./MapResults.css";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 class MapResults extends React.Component {
+  state = {
+    map: null
+  };
+
+  componentDidMount() {
+    var map = new mapboxgl.Map({
+      container: "results-map",
+      style: "mapbox://styles/mapbox/streets-v11"
+    });
+
+    this.setState({ map: map });
+  }
 
   render() {
-    return null;
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.viewContainer}>
+        <div id="results-map" style={{height: 'calc(100vh - 183px'}}></div>;
+      </div>
+    );
   }
 }
 
-export default MapResults;
+export default withStyles(styles)(MapResults);
