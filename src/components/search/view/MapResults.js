562b67c5a7f13eefe1860df2c02a26072ebcb045
mezdru <quentin.drumez@isen.yncrea.fr>
2019-12-23 12:38:14 +0100

    Zoom to expand cluster on click on cluster

--- src/components/search/view/MapResults.js
+++ src/components/search/view/MapResults.js
@@ -60,7 +60,7 @@ class MapResults extends React.Component {
       }
     });
 
-    this.map.on("data", (e) => {
+    this.map.on("data", e => {
       if (e.sourceId !== "addresses" || !e.isSourceLoaded) return;
       this.map.on("moveend", this.updateMarkers); // moveend also fires on zoomend
       this.updateMarkers();
@@ -68,7 +68,7 @@ class MapResults extends React.Component {
   };
 
   updateMarkers = () => {
-    console.log('update markers');
+    console.log(">>>>>>>>>>>>>>>>>> update markers");
     const features = this.map.querySourceFeatures("addresses");
     const keepMarkers = [];
 
@@ -79,38 +79,41 @@ class MapResults extends React.Component {
 
       const clusterID = props.cluster_id || null;
 
-      console.log(`${featureID} ${props.cluster}`, props);
-
+      console.log(`${featureID || clusterID} ${props.cluster}`, props);
 
       if (props.cluster && this.markers.has("cluster_" + clusterID)) {
-        console.log(" cluster already on screen")
+        // console.log(" cluster already on screen");
         //Cluster marker is already on screen
         keepMarkers.push("cluster_" + clusterID);
       } else if (props.cluster) {
-        console.log("create cluster")
+        // console.log("create cluster");
         //This feature is clustered, create an icon for it and use props.point_count for its count
-        var size = 50 + 5*Math.log1p(props.point_count);
+        var size = 50 + 5 * Math.log1p(props.point_count);
         var el = document.createElement("div");
         el.className = "mapCluster";
         el.style.width = `${size}px`;
         el.style.height = `${size}px`;
-        el.innerHTML = `<div>${props.point_count}</div>`
+        el.dataset.type = "cluster";
+        el.innerHTML = `<div>${props.point_count}</div>`;
         const marker = new mapboxgl.Marker(el).setLngLat(coords);
         marker.addTo(this.map);
         keepMarkers.push("cluster_" + clusterID);
         this.markers.set("cluster_" + clusterID, el);
+        el.addEventListener("click", e =>
+          this.handleClusterClick(e, clusterID, features[i])
+        );
       } else if (this.markers.has(featureID)) {
-        console.log("marker already on screen")
+        // console.log("marker already on screen");
         //Feature marker is already on screen
         keepMarkers.push(featureID);
       } else {
-        console.log("create marker")
+        // console.log("create marker");
         //Feature is not clustered and has not been created, create an icon for it
         const el = document.createElement("div");
         // el.style.backgroundImage = "url(https://placekitten.com/g/50/50)";
         el.className = "marker";
         el.innerHTML = `<i class="fa fa-map-marker" aria-hidden="true"></i><img src="${defaultProfilePicture}" />`;
-        el.dataset.type = props.type;
+        el.dataset.type = "marker";
         const marker = new mapboxgl.Marker(el).setLngLat(coords);
         marker.addTo(this.map);
         keepMarkers.push(props.id);
@@ -120,10 +123,10 @@ class MapResults extends React.Component {
 
     //Let's clean-up any old markers. Loop through all markers
     this.markers.forEach((value, key, map) => {
-      console.log(key, keepMarkers.indexOf(key));
+      // console.log(key, keepMarkers.indexOf(key));
       //If marker exists but is not in the keep array
       if (keepMarkers.indexOf(key) === -1) {
-        console.log("deleting key: " + key);
+        // console.log("deleting key: " + key);
         //Remove it from the page
         value.remove();
         //Remove it from markers map
@@ -132,164 +135,38 @@ class MapResults extends React.Component {
     });
   };
 
-  customMarker = () => {
-    console.log(defaultProfilePicture);
-    var div = document.createElement("div");
-    div.className = "marker";
-    div.innerHTML = `<i class="fa fa-map-marker" aria-hidden="true"></i><img src="${defaultProfilePicture}" />`;
-    return div;
-
-    // let output = (
-    //   <div className="marker">
-    //     <img src={defaultProfilePicture} />
-    //   </div>
-    // );
-
-    // output.setClassName("marker");
-    // return output;
+  flyIntoCluster = async (coordinates, currentZoom, wantedZoom) => {
+    this.map.flyTo({
+      center: coordinates,
+      zoom: wantedZoom + 0.3,
+      bearing: 0,
+      speed: 1.5,
+      easing: function(t) {
+        if (t <= 0.5) return 2.0 * Math.pow(t, 2);
+        t -= 0.5;
+        return 2.0 * t * (1.0 - t) + 0.5;
+      }
+    });
   };
 
-  setAddons = () => {
-    // set locale
-    var mapboxLanguage = new MapboxLanguage();
-    this.map.setStyle(
-      mapboxLanguage.setLanguage(
-        this.map.getStyle(),
-        this.props.commonStore.locale
-      )
+  handleClusterClick = (event, clusterID, data) => {
+    const clusters = this.map.queryRenderedFeatures(event.point, {
+      layers: ["clusters"]
+    });
+    const wantedCluster = clusters.find(
+      elt => elt.properties.cluster_id === clusterID
     );
+    const coordinates = wantedCluster.geometry.coordinates;
+    const currentZoom = this.map.getZoom();
 
-    // add geocoder
-    var geocoder = new MapboxGeocoder({
-      accessToken: mapboxgl.accessToken,
-      mapboxgl: mapboxgl
-    });
-
-    document
-      .getElementById("input-geocoder")
-      .appendChild(geocoder.onAdd(this.map));
-
-    this.map.addSource("records", {
-      type: "geojson",
-      cluster: true,
-      clusterRadius: 30,
-      data: {
-        type: "FeatureCollection",
-        features: [
-          {
-            // feature for Mapbox DC
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [2.949014, 50.265924]
-            }
-          },
-          {
-            // feature for Mapbox DC
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [2.549014, 51.265924]
-            }
-          },
-          {
-            // feature for Mapbox DC
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [3.949014, 49.265924]
-            }
-          },
-          {
-            // feature for Mapbox DC
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [3.449014, 49.265924]
-            }
-          },
-          {
-            // feature for Mapbox DC
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [3.949014, 48.265924]
-            }
-          },
-          {
-            // feature for Mapbox DC
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [4.249014, 48.565924]
-            }
-          },
-          {
-            // feature for Mapbox DC
-            type: "Feature",
-            geometry: {
-              type: "Point",
-              coordinates: [4.449014, 9.265924]
-            }
-          }
-        ]
-      }
-    });
-
-    this.map.addLayer({
-      id: "clusters",
-      type: "circle",
-      source: "records",
-      paint: {
-        "circle-stroke-color": [
-          "step",
-          ["get", "point_count"],
-          "#51bbd6",
-          2,
-          "#f1f075",
-          3,
-          "#f28cb1"
-        ],
-        "circle-stroke-width": 5,
-        "circle-color": "rgba(0,0,0,.7)",
-        "circle-radius": ["step", ["get", "point_count"], 20, 2, 40, 3, 60]
-      },
-      filter: ["has", "point_count"]
-    });
-
-    this.map.addLayer({
-      id: "cluster-count",
-      type: "symbol",
-      source: "records",
-      filter: ["has", "point_count"],
-      layout: {
-        "text-field": "{point_count_abbreviated}",
-        "text-font": ["DIN Offc Pro Medium", "Arial Unicode MS Bold"],
-        "text-size": 30
-      },
-      paint: {
-        "text-color": "#ffffff"
-      }
-    });
-
-    this.map.addLayer({
-      id: "unclustered-point",
-      type: "circle",
-      source: "records",
-      filter: ["!", ["has", "point_count"]],
-      paint: {
-        "circle-color": "#11b4da",
-        "circle-radius": 10,
-        "circle-stroke-width": 1,
-        "circle-stroke-color": "#fff"
-      }
-    });
-  };
-
-  addRecordMarker = (record, lat, lng) => {
-    let marker = new mapboxgl.Marker({ element: this.customMarker() })
-      .setLngLat([lng, lat])
-      .addTo(this.map);
+    this.map
+      .getSource("addresses")
+      .getClusterExpansionZoom(
+        wantedCluster.properties.cluster_id,
+        (e, zoom) => {
+          this.flyIntoCluster(coordinates, currentZoom, zoom);
+        }
+      );
   };
 
   // use it in onboard ?
