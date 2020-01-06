b26f5a38f762838ffe275c10fdb70e6a57b4389e
mezdru <quentin.drumez@isen.yncrea.fr>
2020-01-06 10:53:09 +0100

    Geocoding field : link to api

--- src/components/utils/fields/GeocodingField.js
+++ src/components/utils/fields/GeocodingField.js
@@ -2,6 +2,7 @@ import React from "react";
 import withMapbox from "../../../hoc/MapboxManagement.hoc";
 import { inject, observer } from "mobx-react";
 import { withStyles } from "@material-ui/core";
+import request from "async-request";
 
 const styles = theme => ({
   root: {
@@ -19,29 +20,47 @@ const styles = theme => ({
     position: "absolute",
     zIndex: 1,
     width: "calc(100% - 32px)",
-    left:0,
+    left: 0,
     right: 0,
     margin: "auto",
     top: 16,
-    '& > div': {
-      width: '100%',
-      maxWidth: 'inherit',
+    "& > div": {
+      width: "100%",
+      maxWidth: "inherit"
     }
   }
 });
 
 class GeocodingField extends React.Component {
+  state = {
+    lat: null,
+    lng: null
+  };
+
   componentDidMount() {
     this.props.mapbox.buildMap(this.mapContainer).then(map => {
       this.map = map;
       this.props.mapbox.setLocale(this.map, this.props.commonStore.locale);
-      this.geocoder = this.props.mapbox.addGeocoder(this.map, this.geocoderContainer);
+      this.geocoder = this.props.mapbox.addGeocoder(
+        this.map,
+        this.geocoderContainer
+      );
 
-      this.geocoder.on('result', (res) => {
-        console.log('result')
-        console.log(res.result.place_name);
-        console.log(res.result.geometry.coordinates); // coordinates selected by the user
-        // at the end, need a call to the mapbox permanent endpoint in order to persist the data
+      this.geocoder.on("result", res => {
+        this.setState(
+          {
+            lat: res.result.geometry.coordinates[1],
+            lng: res.result.geometry.coordinates[0]
+          },
+          async () => {
+            let response = await request(
+              `https://api.mapbox.com/geocoding/v5/mapbox.places-permanent/${this.state.longitude},${this.state.latitude}.json?access_token=${process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}`
+            );
+            console.log(response);
+            this.props.onChange({ lat: this.state.lat, lng: this.state.lng });
+            if(this.props.handleSave) this.props.handleSave(["_geoloc"]);
+          }
+        );
       });
     });
   }
