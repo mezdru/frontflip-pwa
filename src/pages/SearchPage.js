409ef00a7f7d7e4248568dd1f0a4252ae2b86a7a
mezdru <quentin.drumez@isen.yncrea.fr>
2020-01-08 15:07:54 +0100

    Fix : header css on mobile when list view

--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -83,7 +83,7 @@ class SearchPage extends React.Component {
     return (
       <>
         <Suspense fallback={<></>}>
-          <Header withProfileLogo forceInside={this.props.width === "xs"} />
+          <Header withProfileLogo forceInside={view === VIEW_MAP && this.props.width === "xs"} />
         </Suspense>
 
         <main className={classes.searchContainer}>
