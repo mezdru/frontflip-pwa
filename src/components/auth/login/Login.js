4c5d462dcb9c8ab63621899b3df20a6c8deb3a07
Matthieu Kluj <matthieu.kluj@gmail.com>
2020-02-13 10:49:13 +0100

    feat(auth): toggle login and signup providers with organisation config (#997)
    * chore(eslint): remove custom eslintrc
    
    * feat(auth): toggle login and signup providers with organisation config

--- src/components/auth/login/Login.js
+++ src/components/auth/login/Login.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { FormattedMessage, injectIntl, FormattedHTMLMessage } from 'react-intl';
-import { Link, Redirect, withRouter } from 'react-router-dom';
-import { inject, observer } from 'mobx-react';
+import React from "react";
+import { FormattedMessage, injectIntl, FormattedHTMLMessage } from "react-intl";
+import { Link, Redirect, withRouter } from "react-router-dom";
+import { inject, observer } from "mobx-react";
+import { getOr } from "lodash/fp";
 
-import { Button, CircularProgress, Grid, TextField, Typography } from '@material-ui/core';
-import SnackbarCustom from '../../utils/snackbars/SnackbarCustom';
-import ReactGA from 'react-ga';
-import LogRocket from 'logrocket';
-import IntegrationButton from '../../utils/buttons/IntegrationButton';
-import {getBaseUrl} from '../../../services/utils.service';
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  TextField,
+  Typography
+} from "@material-ui/core";
+import SnackbarCustom from "../../utils/snackbars/SnackbarCustom";
+import ReactGA from "react-ga";
+import LogRocket from "logrocket";
+import IntegrationButton from "../../utils/buttons/IntegrationButton";
+import { getBaseUrl } from "../../../services/utils.service";
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
 
@@ -28,70 +35,103 @@ class Login extends React.Component {
 
     // In this case, the user try to Sign In with an integration (LinkedIn, ...) and we do not already have an account for him.
     // We will ask him to Sign In with an other method, then, we will link the integration to his account.
-    if (authState && authState.action === 'signin' && authState.success === 'false') {
-      let integrationName = (authState.integration ? authState.integration.charAt(0).toUpperCase() + authState.integration.slice(1) : '');
+    if (
+      authState &&
+      authState.action === "signin" &&
+      authState.success === "false"
+    ) {
+      let integrationName = authState.integration
+        ? authState.integration.charAt(0).toUpperCase() +
+          authState.integration.slice(1)
+        : "";
       this.setState({
         loginErrors: this.props.intl.formatMessage(
-          { id: 'signin.error.integration.notFound' },
+          { id: "signin.error.integration.notFound" },
           {
             integrationName: integrationName,
-            signupLink: getBaseUrl(this.props) + '/signup'
+            signupLink: getBaseUrl(this.props) + "/signup"
           }
         )
       });
     }
-
   }
 
-  handleEmailChange = (e) => {
+  handleEmailChange = e => {
     this.props.authStore.setEmail(e.target.value);
     this.forceUpdate();
   };
 
-  handlePasswordChange = (e) => {
+  handlePasswordChange = e => {
     this.props.authStore.setPassword(e.target.value);
     this.forceUpdate();
   };
 
   signinSuccessRedirect = () => {
     let defaultRedirect = getBaseUrl(this.props);
-    let wantedRedirect = this.props.commonStore.getCookie('wantedPath');
-    if (wantedRedirect) this.props.commonStore.removeCookie('wantedPath');
+    let wantedRedirect = this.props.commonStore.getCookie("wantedPath");
+    if (wantedRedirect) this.props.commonStore.removeCookie("wantedPath");
     this.setState({ redirectTo: wantedRedirect || defaultRedirect });
-  }
+  };
 
-  handleSubmitForm = (e) => {
+  handleSubmitForm = e => {
     e.preventDefault();
-    this.props.authStore.login()
-      .then((response) => {
+    this.props.authStore
+      .login()
+      .then(response => {
         if (response === 200) {
-          ReactGA.event({ category: 'User', action: 'Login with password' });
-          LogRocket.info('User login with password.');
+          ReactGA.event({ category: "User", action: "Login with password" });
+          LogRocket.info("User login with password.");
           this.signinSuccessRedirect();
         } else {
-          this.setState({ loginErrors: this.props.intl.formatMessage({ id: 'signin.error.generic' }) });
+          this.setState({
+            loginErrors: this.props.intl.formatMessage({
+              id: "signin.error.generic"
+            })
+          });
         }
-      }).catch((err) => {
+      })
+      .catch(err => {
         this.setState({ loginErrors: this.getSigninErrorMessage(err) });
       });
   };
 
-  getSigninErrorMessage = (err) => {
+  getSigninErrorMessage = err => {
     let errorMessage;
     if (err.status === 404) {
-      errorMessage = this.props.intl.formatMessage({ id: 'signin.error.unknown' });
+      errorMessage = this.props.intl.formatMessage({
+        id: "signin.error.unknown"
+      });
     } else if (err.status === 403) {
-      if (err.response.body.error_description === 'Wrong password.') {
-        errorMessage = this.props.intl.formatMessage({ id: 'signin.error.wrongPassword' }, { forgotPasswordLink: '/' + this.props.commonStore.locale + '/password/forgot' });
-      } else if (err.response.body.error_description === 'User use Google Auth.') {
-        errorMessage = this.props.intl.formatMessage({ id: 'signin.error.useGoogle' });
+      if (err.response.body.error_description === "Wrong password.") {
+        errorMessage = this.props.intl.formatMessage(
+          { id: "signin.error.wrongPassword" },
+          {
+            forgotPasswordLink:
+              "/" + this.props.commonStore.locale + "/password/forgot"
+          }
+        );
+      } else if (
+        err.response.body.error_description === "User use Google Auth."
+      ) {
+        errorMessage = this.props.intl.formatMessage({
+          id: "signin.error.useGoogle"
+        });
       } else {
-        errorMessage = this.props.intl.formatMessage({ id: 'signin.error.noPassword' }, { forgotPasswordLink: '/' + this.props.commonStore.locale + '/password/forgot' });
+        errorMessage = this.props.intl.formatMessage(
+          { id: "signin.error.noPassword" },
+          {
+            forgotPasswordLink:
+              "/" + this.props.commonStore.locale + "/password/forgot"
+          }
+        );
       }
     }
-    if (!errorMessage) errorMessage = this.props.intl.formatMessage({ id: 'signin.error.generic' });
+    if (!errorMessage)
+      errorMessage = this.props.intl.formatMessage({
+        id: "signin.error.generic"
+      });
     return errorMessage;
-  }
+  };
 
   render() {
     const { values, inProgress } = this.props.authStore;
@@ -100,28 +140,48 @@ class Login extends React.Component {
     const { locale } = this.props.commonStore;
     let intl = this.props.intl;
 
+    const loginProviders = getOr(
+      [],
+      "settings.auth.providers"
+    )(currentOrganisation);
+
     if (redirectTo) {
       return <Redirect push to={redirectTo} />;
-    }
-    else if (isAuth) {
-      return <Redirect push to={'/' + locale + (currentOrganisation ? '/' + currentOrganisation.tag : '')} />;
-    }
-    else {
+    } else if (isAuth) {
+      return (
+        <Redirect
+          push
+          to={
+            "/" +
+            locale +
+            (currentOrganisation ? "/" + currentOrganisation.tag : "")
+          }
+        />
+      );
+    } else {
       return (
         <form onSubmit={this.handleSubmitForm} id="form-login">
-          <Grid container item direction='column' spacing={2}>
+          <Grid container item direction="column" spacing={2}>
             {loginErrors && (
               <Grid item>
                 <SnackbarCustom variant="warning" message={loginErrors} />
               </Grid>
             )}
-            <Grid item container justify='center' style={{ textAlign: 'center' }}>
+            <Grid
+              item
+              container
+              justify="center"
+              style={{ textAlign: "center" }}
+            >
               <Typography variant="h1">
                 <FormattedHTMLMessage id="signin.title.org" />
               </Typography>
             </Grid>
-            <Grid item justify={'center'} container>
-              <Button variant="text" onClick={() => this.props.handleChangeIndex(1)} >
+            <Grid item justify={"center"} container>
+              <Button
+                variant="text"
+                onClick={() => this.props.handleChangeIndex(1)}
+              >
                 <FormattedHTMLMessage id="signin.or.create" />
               </Button>
             </Grid>
@@ -138,7 +198,7 @@ class Login extends React.Component {
             </Grid>
             <Grid item>
               <TextField
-                label={intl.formatMessage({ id: 'Password' })}
+                label={intl.formatMessage({ id: "Password" })}
                 type="password"
                 autoComplete="current-password"
                 fullWidth
@@ -147,48 +207,83 @@ class Login extends React.Component {
                 required
               />
             </Grid>
-            <Grid item container justify={'center'}>
-              {
-                inProgress && (
-                  <CircularProgress color="secondary" />
-                )
-              }
-              {
-                !inProgress && (
-                  <Button fullWidth={true} color="secondary" variant="contained" type="submit"><FormattedMessage id="Sign In" /></Button>
-                )
-              }
+            <Grid item container justify={"center"}>
+              {inProgress && <CircularProgress color="secondary" />}
+              {!inProgress && (
+                <Button
+                  fullWidth={true}
+                  color="secondary"
+                  variant="contained"
+                  type="submit"
+                >
+                  <FormattedMessage id="Sign In" />
+                </Button>
+              )}
             </Grid>
             <Grid item container justify="center">
-              <Button component={Link}
-                to={"/" + locale + (currentOrganisation ? '/' + currentOrganisation.tag : '') + "/password/forgot"}
+              <Button
+                component={Link}
+                to={
+                  "/" +
+                  locale +
+                  (currentOrganisation ? "/" + currentOrganisation.tag : "") +
+                  "/password/forgot"
+                }
                 variant="text"
               >
                 <FormattedMessage id="I don't have my password" />
               </Button>
             </Grid>
-            <Grid item>
-              <Typography style={{
-                fontSize: '1rem',
-                color: '#7c7c7c',
-                textAlign: 'center'
-              }}>
-                <FormattedHTMLMessage id="signin.or.integration" />
-              </Typography>
-            </Grid>
-            <Grid container item direction="row" justify="center" spacing={2} >
-              <Grid item><IntegrationButton labelId={"Sign in with Google"} integrationTag="google" /></Grid>
-              <Grid item><IntegrationButton labelId={"Sign in with Google"} integrationTag="linkedin" currentAction="signin" /></Grid>
-            </Grid>
+            {loginProviders.length > 0 && (
+              <>
+                <Grid item>
+                  <Typography
+                    style={{
+                      fontSize: "1rem",
+                      color: "#7c7c7c",
+                      textAlign: "center"
+                    }}
+                  >
+                    <FormattedHTMLMessage id="signin.or.integration" />
+                  </Typography>
+                </Grid>
+                <Grid
+                  container
+                  item
+                  direction="row"
+                  justify="center"
+                  spacing={2}
+                >
+                  {loginProviders.includes("google") && (
+                    <Grid item>
+                      <IntegrationButton
+                        labelId={"Sign in with Google"}
+                        integrationTag="google"
+                      />
+                    </Grid>
+                  )}
+                  {loginProviders.includes("linkedin") && (
+                    <Grid item>
+                      <IntegrationButton
+                        labelId={"Sign in with LinkedIn"}
+                        integrationTag="linkedin"
+                        currentAction="signin"
+                      />
+                    </Grid>
+                  )}
+                </Grid>
+              </>
+            )}
           </Grid>
         </form>
-      )
+      );
     }
-  };
+  }
 }
 
-export default inject('authStore', 'userStore', 'orgStore', 'commonStore')(
-  injectIntl(observer(
-    withRouter(Login)
-  ))
-);
+export default inject(
+  "authStore",
+  "userStore",
+  "orgStore",
+  "commonStore"
+)(injectIntl(observer(withRouter(Login))));
--- src/components/auth/register/Register.js
+++ src/components/auth/register/Register.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { FormattedMessage, injectIntl, FormattedHTMLMessage } from 'react-intl';
-import { inject, observer } from 'mobx-react';
-import { Redirect } from 'react-router-dom';
+import React from "react";
+import { FormattedMessage, injectIntl, FormattedHTMLMessage } from "react-intl";
+import { inject, observer } from "mobx-react";
+import { Redirect } from "react-router-dom";
+import { getOr } from 'lodash/fp';
 
-import { Button, CircularProgress, Grid, TextField, Typography } from "@material-ui/core";
-import SnackbarCustom from '../../utils/snackbars/SnackbarCustom';
-import ReactGA from 'react-ga';
-import LogRocket from 'logrocket';
-import IntegrationButton from '../../utils/buttons/IntegrationButton';
-import { getBaseUrl } from '../../../services/utils.service';
+import {
+  Button,
+  CircularProgress,
+  Grid,
+  TextField,
+  Typography
+} from "@material-ui/core";
+import SnackbarCustom from "../../utils/snackbars/SnackbarCustom";
+import ReactGA from "react-ga";
+import LogRocket from "logrocket";
+import IntegrationButton from "../../utils/buttons/IntegrationButton";
+import { getBaseUrl } from "../../../services/utils.service";
 
 ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_ID);
 
@@ -19,32 +26,34 @@ class Register extends React.Component {
       value: 0,
       registerErrors: null,
       registerSuccess: false,
-      registerSuccessMessage: '',
+      registerSuccessMessage: ""
     };
   }
 
-  handleEmailChange = (e) => {
+  handleEmailChange = e => {
     this.props.authStore.setEmail(e.target.value);
   };
 
-  handlePasswordChange = (e) => {
-    this.props.authStore.setPassword(e.target.value)
+  handlePasswordChange = e => {
+    this.props.authStore.setPassword(e.target.value);
   };
 
-  handleSubmitForm = (e) => {
+  handleSubmitForm = e => {
     e.preventDefault();
-    this.props.authStore.register()
+    this.props.authStore
+      .register()
       .then(() => {
-
-        ReactGA.event({ category: 'User', action: 'Register with password' });
-        LogRocket.info('User register with password.');
+        ReactGA.event({ category: "User", action: "Register with password" });
+        LogRocket.info("User register with password.");
 
         if (this.props.commonStore.url.params.orgTag) {
-
-          this.props.authStore.registerToOrg()
-            .then((data) => {
-
-              ReactGA.event({ category: 'User', action: 'Register to Organisation' });
+          this.props.authStore
+            .registerToOrg()
+            .then(data => {
+              ReactGA.event({
+                category: "User",
+                action: "Register to Organisation"
+              });
 
               let cUser = this.props.userStore.currentUser;
 
@@ -53,114 +62,167 @@ class Register extends React.Component {
                 this.setState({ redirectTo: getBaseUrl(this.props) });
               } else {
                 // New User, shows success message.
-                this.setState(
-                  {
-                    registerSuccess: true,
-                    registerSuccessMessage: this.props.intl.formatMessage({ id: 'signup.success' })
-                  },
-                );
+                this.setState({
+                  registerSuccess: true,
+                  registerSuccessMessage: this.props.intl.formatMessage({
+                    id: "signup.success"
+                  })
+                });
               }
-
-            }).catch((err) => {
-              if(err.response && err.response.body && err.response.body.message === 'Bad invitation code') {
-                this.setState({registerWarning: true});
-                this.setState({registerWarningMessage: this.props.intl.formatMessage({ id: 'signup.warning.badInvitationCode' }, { orgName: this.props.orgStore.currentOrganisation.name }) });
+            })
+            .catch(err => {
+              if (
+                err.response &&
+                err.response.body &&
+                err.response.body.message === "Bad invitation code"
+              ) {
+                this.setState({ registerWarning: true });
+                this.setState({
+                  registerWarningMessage: this.props.intl.formatMessage(
+                    { id: "signup.warning.badInvitationCode" },
+                    { orgName: this.props.orgStore.currentOrganisation.name }
+                  )
+                });
               } else {
                 this.setState({ registerSuccess: true });
-                this.setState({ registerSuccessMessage: this.props.intl.formatMessage({ id: 'signup.warning.forbiddenOrg' }, { orgName: this.props.orgStore.currentOrganisation.name }) });
+                this.setState({
+                  registerSuccessMessage: this.props.intl.formatMessage(
+                    { id: "signup.warning.forbiddenOrg" },
+                    { orgName: this.props.orgStore.currentOrganisation.name }
+                  )
+                });
               }
             });
         } else {
-
-          this.setState({ registerSuccessMessage: this.props.intl.formatMessage({ id: 'signup.success' }) });
+          this.setState({
+            registerSuccessMessage: this.props.intl.formatMessage({
+              id: "signup.success"
+            })
+          });
           this.setState({ registerSuccess: true });
-
         }
-
-      }).catch((err) => {
-
+      })
+      .catch(err => {
         let errorMessage;
 
         if (err.status === 422) {
-
           err.response.body.errors.forEach(error => {
-
-            if (error.param === 'password') {
-
-              if (error.type === 'dumb') {
-
+            if (error.param === "password") {
+              if (error.type === "dumb") {
                 // (frequency over 100 000 passwords, 3 000 000 000 people use internet, 30 000 = 3 000 000 000 / 100 000)
-                errorMessage = (errorMessage ? errorMessage + '<br/>' : '') + this.props.intl.formatMessage({ id: 'signup.error.dumbPassword' }, { dumbCount: (parseInt(error.msg) * 30000).toLocaleString() });
-
+                errorMessage =
+                  (errorMessage ? errorMessage + "<br/>" : "") +
+                  this.props.intl.formatMessage(
+                    { id: "signup.error.dumbPassword" },
+                    {
+                      dumbCount: (parseInt(error.msg) * 30000).toLocaleString()
+                    }
+                  );
               } else {
-
-                errorMessage = (errorMessage ? errorMessage + '<br/>' : '') + this.props.intl.formatMessage({ id: 'signup.error.shortPassword' });
+                errorMessage =
+                  (errorMessage ? errorMessage + "<br/>" : "") +
+                  this.props.intl.formatMessage({
+                    id: "signup.error.shortPassword"
+                  });
               }
-
-            } else if (error.param === 'email') {
-
-              errorMessage = (errorMessage ? errorMessage + '<br/>' : '') + this.props.intl.formatMessage({ id: 'signup.error.wrongEmail' });
-
+            } else if (error.param === "email") {
+              errorMessage =
+                (errorMessage ? errorMessage + "<br/>" : "") +
+                this.props.intl.formatMessage({
+                  id: "signup.error.wrongEmail"
+                });
             }
           });
-
-        } else if (err.status === 400 && err.response.body.message === 'User already exists.') {
-
-          errorMessage = this.props.intl.formatMessage({ id: 'signup.error.userExists' }, { forgotPasswordLink: '/' + this.props.commonStore.locale + '/password/forgot' });
-
+        } else if (
+          err.status === 400 &&
+          err.response.body.message === "User already exists."
+        ) {
+          errorMessage = this.props.intl.formatMessage(
+            { id: "signup.error.userExists" },
+            {
+              forgotPasswordLink:
+                "/" + this.props.commonStore.locale + "/password/forgot"
+            }
+          );
         }
 
-        if (!errorMessage) errorMessage = this.props.intl.formatMessage({ id: 'signup.error.generic' });
+        if (!errorMessage)
+          errorMessage = this.props.intl.formatMessage({
+            id: "signup.error.generic"
+          });
         this.setState({ registerErrors: errorMessage });
-
       });
   };
 
   render() {
     const { values, inProgress } = this.props.authStore;
-    let { registerErrors, registerSuccess, registerSuccessMessage, redirectTo, registerWarning, registerWarningMessage } = this.state;
+    const { currentOrganisation } = this.props.orgStore;
+    let {
+      registerErrors,
+      registerSuccess,
+      registerSuccessMessage,
+      redirectTo,
+      registerWarning,
+      registerWarningMessage
+    } = this.state;
     let intl = this.props.intl;
 
-    console.debug('%c Render Register.js', 'background-color: grey; padding: 6px 12px; border-radius: 5px; color: white;');
+    const loginProviders = getOr(
+      [],
+      "settings.auth.providers"
+    )(currentOrganisation);
+
+    console.debug(
+      "%c Render Register.js",
+      "background-color: grey; padding: 6px 12px; border-radius: 5px; color: white;"
+    );
 
     if (redirectTo) return <Redirect push to={redirectTo} />;
 
     if (registerSuccess) {
       return (
-        <Grid container item direction='column' spacing={2}>
+        <Grid container item direction="column" spacing={2}>
           <Grid item>
-            <SnackbarCustom variant="success" message={registerSuccessMessage} />
+            <SnackbarCustom
+              variant="success"
+              message={registerSuccessMessage}
+            />
           </Grid>
         </Grid>
-      )
+      );
     } else if (registerWarning) {
       return (
-        <Grid container item direction='column' spacing={2}>
+        <Grid container item direction="column" spacing={2}>
           <Grid item>
-            <SnackbarCustom variant="warning" message={registerWarningMessage} />
+            <SnackbarCustom
+              variant="warning"
+              message={registerWarningMessage}
+            />
           </Grid>
         </Grid>
-      )
+      );
     } else {
       return (
         <form onSubmit={this.handleSubmitForm} id="form-register">
-          <Grid container item direction='column' spacing={2}>
+          <Grid container item direction="column" spacing={2}>
             {registerErrors && (
               <Grid item>
                 <SnackbarCustom variant="warning" message={registerErrors} />
               </Grid>
             )}
-            <Grid item container justify='center'>
-              <Typography variant="h1" style={{ textAlign: 'center' }}>
+            <Grid item container justify="center">
+              <Typography variant="h1" style={{ textAlign: "center" }}>
                 <FormattedHTMLMessage id="signup.title.org" />
               </Typography>
             </Grid>
-            <Grid item container justify='center'>
-              <Button variant="text" onClick={() => this.props.handleChangeIndex(0)} >
+            <Grid item container justify="center">
+              <Button
+                variant="text"
+                onClick={() => this.props.handleChangeIndex(0)}
+              >
                 <FormattedHTMLMessage id="signup.or.signin" />
               </Button>
             </Grid>
-
 
             <Grid item>
               <TextField
@@ -175,7 +237,7 @@ class Register extends React.Component {
             </Grid>
             <Grid item>
               <TextField
-                label={intl.formatMessage({ id: 'Password' })}
+                label={intl.formatMessage({ id: "Password" })}
                 type="password"
                 autoComplete="current-password"
                 fullWidth
@@ -184,38 +246,68 @@ class Register extends React.Component {
                 required
               />
             </Grid>
-            <Grid item container justify={'center'}>
-              {
-                inProgress && (
-                  <CircularProgress color="secondary" />
-                )
-              }
-              {
-                !inProgress && (
-                  <Button fullWidth type="submit" variant="contained" color="secondary"><FormattedMessage id="Sign Up" /></Button>
-                )
-              }
+            <Grid item container justify={"center"}>
+              {inProgress && <CircularProgress color="secondary" />}
+              {!inProgress && (
+                <Button
+                  fullWidth
+                  type="submit"
+                  variant="contained"
+                  color="secondary"
+                >
+                  <FormattedMessage id="Sign Up" />
+                </Button>
+              )}
             </Grid>
-            <Grid item>
-              <Typography style={{
-                fontSize: '1rem',
-                color: '#7c7c7c',
-                textAlign: 'center'
-              }}><FormattedHTMLMessage id="signup.or.integration" /></Typography>
-            </Grid>
-            <Grid container item direction="row" justify="center" spacing={2} >
-              <Grid item><IntegrationButton labelId={"Sign in with Google"} integrationTag="google" /></Grid>
-              <Grid item><IntegrationButton labelId={"Sign in with Google"} integrationTag="linkedin" /></Grid>
-            </Grid>
+            {loginProviders.length > 0 && (
+              <>
+                <Grid item>
+                  <Typography
+                    style={{
+                      fontSize: "1rem",
+                      color: "#7c7c7c",
+                      textAlign: "center"
+                    }}
+                  >
+                    <FormattedHTMLMessage id="signup.or.integration" />
+                  </Typography>
+                </Grid>
+                <Grid
+                  container
+                  item
+                  direction="row"
+                  justify="center"
+                  spacing={2}
+                >
+                  {loginProviders.includes("google") && (
+                    <Grid item>
+                      <IntegrationButton
+                        labelId={"Sign in with Google"}
+                        integrationTag="google"
+                      />
+                    </Grid>
+                  )}
+                  {loginProviders.includes("linkedin") && (
+                    <Grid item>
+                      <IntegrationButton
+                        labelId={"Sign in with Google"}
+                        integrationTag="linkedin"
+                      />
+                    </Grid>
+                  )}
+                </Grid>
+              </>
+            )}
           </Grid>
         </form>
-      )
+      );
     }
-  };
+  }
 }
 
-export default inject('authStore', 'userStore', 'orgStore', 'commonStore')(
-  injectIntl(observer(
-    Register
-  ))
-);
+export default inject(
+  "authStore",
+  "userStore",
+  "orgStore",
+  "commonStore"
+)(injectIntl(observer(Register)));
